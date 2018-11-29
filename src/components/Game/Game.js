34fe5fc595a69fe135a86f64a27c388559fde84e
metamn <bartus.csongor@gmail.com>
2018-11-29 19:16:26 +0200

    Storybook added

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
+/**
+ * The main class 
+ */
 export default class Game extends React.Component {
 	render() {
 		return (
