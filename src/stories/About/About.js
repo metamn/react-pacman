095685b3e1c9a6f300400e4a9a2e6d3217e1d807
metamn <bartus.csongor@gmail.com>
2018-11-29 19:32:45 +0200

    First storybook entry added

---
+++ src/stories/About/About.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default () =>
+	<div class="container">
+		<h1>Welcome</h1>
+	</div>
---
+++ src/stories/About/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import About from './About.js';
+
+storiesOf('About')
+	.add('AboutUs', About);
