5aefd8ca36ae9a985045d9cc26baf1e105f03943
metamn <bartus.csongor@gmail.com>
2018-11-29 18:36:02 +0200

    Create React App run, dependencies installed

---
+++ src/components/Game/Game.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+export default class Game extends React.Component {
+	render() {
+		return (
+			'hello'
+		)
+	}
+}
